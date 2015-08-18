211fc28f054d15a19cac137ecb4cb0acc07ab443
amyble <amymylinh.le@hotmail.co.uk>
2015-08-18 15:11:07 +0100

    Running robot/cleanup.py

--- robot/cleanup.py
+++ robot/cleanup.py
@@ -6,7 +6,7 @@ from tealight.robot import (move,
                             left_side, 
                             right_side)
 while True:
-  for n in range(-1,2):
+  for n in range(-1,2,2):
     while look() == 'fruit' or left_side() == 'fruit' or right_side() == 'fruit':
       move()
     turn(-1)
