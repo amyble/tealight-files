01f1654440e053cf1c90ae9a1bbeeab9de9b3df0
amyble <amymylinh.le@hotmail.co.uk>
2015-08-18 15:19:30 +0100

    Running robot/vine.py

--- robot/vine.py
+++ robot/vine.py
@@ -6,3 +6,11 @@ from tealight.robot import (move,
                             left_side, 
                             right_side)
 
+while True:
+  for n in range(-1,2,2):
+    print n
+    while look() == 'fruit' or left_side() == 'fruit' or right_side() == 'fruit':
+      move()
+    turn(n)
+    move()
+    turn(n)
