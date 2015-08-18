abb1d7d6fce7bcbdbf20f6c3a307d4bc0c64b344
amyble <amymylinh.le@hotmail.co.uk>
2015-08-18 12:03:19 +0100

    Running logo/chessboard.py

--- logo/chessboard.py
+++ logo/chessboard.py
@@ -6,4 +6,13 @@ def square(side):
     turn(90)
     
 
-    
+def waterwheel(edges, size):
+  angle = 360 / edges
+  decoration = size / 2
+  for i in range(0, edges):
+    move(size)
+    square(decoration)
+    turn(angle)
+
+turn(-90)
+waterwheel(12,180)
