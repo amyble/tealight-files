d283caa7bfa98b9720b795b5ba0478871252a3b0
amyble <amymylinh.le@hotmail.co.uk>
2015-08-18 12:31:32 +0100

    Running logo/trianglefactal.py

--- logo/trianglefactal.py
+++ logo/trianglefactal.py
@@ -7,3 +7,22 @@ def triangle(size):
     turn(angle)
     
 triangle(100)
+
+def segment(scale, detail):
+  
+  if detail == 0:
+    move(scale)
+  else:
+    segment(scale / 3.0, detail - 1)
+    turn(-60)
+    segment(scale / 3.0, detail - 1)
+    turn(120)
+    segment(scale / 3.0, detail - 1)
+    turn(-60)
+    segment(scale / 3.0, detail - 1)
+    
+
+turn(90)
+move(-300)
+segment(600,1)
+move(-300)
