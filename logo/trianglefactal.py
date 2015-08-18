4235ec8f52d4c4fae0ed3dcc066601b7bcc1d29a
amyble <amymylinh.le@hotmail.co.uk>
2015-08-18 12:28:09 +0100

    Running logo/trianglefactal.py

--- logo/trianglefactal.py
+++ logo/trianglefactal.py
@@ -1,25 +1,6 @@
 from tealight.logo import move, turn
 
-def triangle(side):
+def square(side):
   for i in range(0,3):
     move(side)
     turn(120)
-
-def segment(scale, detail):
-  
-  if detail == 0:
-    move(scale)
-  else:
-    segment(scale / 3.0, detail - 1)
-    turn(-60)
-    segment(scale / 3.0, detail - 1)
-    turn(120)
-    segment(scale / 3.0, detail - 1)
-    turn(-60)
-    segment(scale / 3.0, detail - 1)
-    
-
-turn(90)
-move(-300)
-segment(600,1)
-move(-300)
