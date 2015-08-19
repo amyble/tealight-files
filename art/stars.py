7a5a85617498de635f31ccee05ea77fb8103a646
amyble <amymylinh.le@hotmail.co.uk>
2015-08-19 09:15:00 +0100

    Running art/stars.py

--- art/stars.py
+++ art/stars.py
@@ -17,5 +17,5 @@ def star(x, y, c, size, spines):
     angle = angle + (2 * pi / spines)
 
 star(400, 400, "blue", 100, 50)
-star(400, 400, "purple", 200, 100)
-star(400, 400, "orange", 125, 30)
+star(400, 400, "purple", 100, 50)
+star(400, 400, "orange", 100, 50)
