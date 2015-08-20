1c850011957866da648b860745d2bd61d1f8a4b9
amyble <amymylinh.le@hotmail.co.uk>
2015-08-20 14:45:03 +0100

    Running art/correct.py

---
+++ art/correct.py
@@ -0,0 +1,6 @@
+from tealight.art import (color, line, spot, rectangle, circle, box, image, text, background)
+
+from tealight.art import (screen_width, screen_height)
+
+rectangle(screen_width / 2, screen_height / 2, 150,30)
+rectangle((screen_width / 2) + 150, screen_height / 2, 150,30)
