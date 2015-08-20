10b2001f3767e52ede1a38045666056144ee4a04
amyble <amymylinh.le@hotmail.co.uk>
2015-08-20 13:55:36 +0100

    Running art/word.py

--- art/word.py
+++ art/word.py
@@ -3,6 +3,6 @@ from tealight.art import (screen_width, screen_height)
 import random
 
 rectangle(10, screen_height - 50, 150, 40)
-text(15, screen_height - 55, "Receive word")
+text(15, screen_height - 50, "Receive word")
 a = ["Dog", "Cat", "Castle", "Voldemort", "Barack Obama", "Snowman", "Aeroplane", "Apple"]
 print(random.choice(a))
