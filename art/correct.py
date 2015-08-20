5340982dcae764b80cfee4b49b1439e42532e7ca
amyble <amymylinh.le@hotmail.co.uk>
2015-08-20 15:02:13 +0100

    Running art/correct.py

--- art/correct.py
+++ art/correct.py
@@ -14,5 +14,9 @@ def stopButton():
     color("red")
     rectangle((screen_width - 180), screen_height - 40, 150,30)
     text(screen_width - 85, screen_height - 35, "STOP")
+    stopMinX = screen_width - 180
+    stopMaxX = screen_width - 30
+    stopMinY = screen_height - 40
+    stopMaxY = screen_height - 10
     
 stopButton()
