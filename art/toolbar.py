dd6cb992ae55cc68fa477cd6a5d96f56c9aabd6c
amyble <amymylinh.le@hotmail.co.uk>
2015-08-20 11:45:07 +0100

    Running art/toolbar.py

--- art/toolbar.py
+++ art/toolbar.py
@@ -3,7 +3,7 @@ from tealight.art import (color, line, spot, circle, box, rectangle, image, text
 rectangle(300,50,30,30)
 line(315, 76, 315, 52)
 rectangle(330,50,30,30)
-box (345, 65, 10, 25)
+box (345, 50, 10, 25)
 rectangle(360,50,30,30)
 spot (345, 65, 5)
 rectangle(390,50,30,30)
