e2e3e4085cda4afa9258fabdc7b21a2676faf974
amyble <amymylinh.le@hotmail.co.uk>
2015-08-20 14:49:02 +0100

    Running art/correct.py

--- art/correct.py
+++ art/correct.py
@@ -5,5 +5,5 @@ from tealight.art import (screen_width, screen_height)
 
 rectangle((screen_width / 2) - 75, screen_height / 2, 150,30)
 rectangle((screen_width / 2) + 95, screen_height / 2, 150,30)
-text((screen_width / 2) - 75, ((screen_height / 2) + 5), "Correct")
+text((screen_width / 2) - 32.5, ((screen_height / 2) + 5), "Correct")
 text((screen_width / 2) + 95, screen_height / 2, "Wrong")
