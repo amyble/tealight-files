ebe950765253f97caae7b30d0f6a16880c8004fd
amyble <amymylinh.le@hotmail.co.uk>
2015-08-20 14:43:44 +0100

    Running logo/correct.py

--- logo/correct.py
+++ logo/correct.py
@@ -1,1 +1,5 @@
-print "A new file!"
+from tealight.art import (color, line, spot, rectangle, circle, box, image, text, background)
+
+from tealight.art import (screen_width, screen_height)
+
+rectangle(screen_width / 2, screen_height / 2,  ,30)
