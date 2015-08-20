8f20012169d09cb4afd13f398f03bca7fc09537d
amyble <amymylinh.le@hotmail.co.uk>
2015-08-20 13:45:31 +0100

    Closing art/word.py

--- art/word.py
+++ art/word.py
@@ -1,5 +1,6 @@
 from tealight.art import (color, line, spot, circle, box, rectangle, image, text, background)
 import random
 
-a = ["Dog", "Cat", "Castle", "Voldemort", "Football", "Ice Cream", "Barack Obama"]
+rectangle 
+a = ["Dog", "Cat", "Castle", "Voldemort", "Football", "Ice Cream", "Barack Obama", "Snowman"]
 print(random.choice(a))
