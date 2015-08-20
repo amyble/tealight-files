2478b9396c6757159d5953911c1b9a52b445f224
amyble <amymylinh.le@hotmail.co.uk>
2015-08-20 14:56:11 +0100

    Running art/correct.py

--- art/correct.py
+++ art/correct.py
@@ -1,11 +1,16 @@
 from tealight.art import (color, line, spot, rectangle, circle, box, image, text, background)
 
 from tealight.art import (screen_width, screen_height)
+def finishButtons():
+  color("green")
+  rectangle((screen_width / 2) - 75, screen_height / 2, 150,30)
+  text((screen_width / 2) - 32.5, ((screen_height / 2) + 5), "Correct")
 
-color("green")
-rectangle((screen_width / 2) - 75, screen_height / 2, 150,30)
-text((screen_width / 2) - 32.5, ((screen_height / 2) + 5), "Correct")
-
-color("red")
-rectangle((screen_width / 2) + 95, screen_height / 2, 150,30)
-text((screen_width / 2) + 132.5, ((screen_height / 2) + 5), "Wrong")
+  color("red")
+  rectangle((screen_width / 2) + 95, screen_height / 2, 150,30)
+  text((screen_width / 2) + 132.5, ((screen_height / 2) + 5), "Wrong")
+  
+def stopButton():  
+    color("red")
+    rectangle((screen_width - 40) + 95, screen_height / 2, 150,30)
+    text((screen_width / 2) + 132.5, ((screen_height / 2) + 5), "STOP")
