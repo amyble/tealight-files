f987ac9cf37b04941dc75aec27e7aac006c4ac5d
amyble <amymylinh.le@hotmail.co.uk>
2015-08-20 14:25:38 +0100

    Running art/word.py

--- art/word.py
+++ art/word.py
@@ -1,8 +1,4 @@
-from tealight.art import (font, color, line, spot, circle, box, rectangle, image, text, background)
-from tealight.art import (screen_width, screen_height)
 import random
 
-rectangle(10, screen_height - 50, 130, 35)
-text(15, screen_height - 45, "Play")
 a = ["Dog", "Cat", "Castle", "Voldemort", "Barack Obama", "Snowman", "Aeroplane", "Apple"]
 print(random.choice(a))
