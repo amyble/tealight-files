a5c09a15cd4396cca1a4d695a3ceea2347b96d28
amyble <amymylinh.le@hotmail.co.uk>
2015-08-21 09:45:04 +0100

    Running art/correct.py

--- art/correct.py
+++ art/correct.py
@@ -13,7 +13,9 @@ def stopButton():
   rectangle((screen_width - 180), screen_height - 40, 150,30)
   text(screen_width - 85, screen_height - 35, "STOP")
   stopMinX = (screen_width - 180)
-  stopMaxX = screen_width - 30
-  stopMinY = screen_height - 40
-  stopMaxY = screen_height - 10
+  stopMaxX = (screen_width - 30)
+  stopMinY = (screen_height - 40)
+  stopMaxY = (screen_height - 10)
   print stopMinX
+
+stopButton()
