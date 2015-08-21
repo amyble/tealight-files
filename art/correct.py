5d9d0ee485c7a5638bf65e80e1d545a9e2a7f3ce
amyble <amymylinh.le@hotmail.co.uk>
2015-08-21 09:34:15 +0100

    Running art/correct.py

--- art/correct.py
+++ art/correct.py
@@ -3,7 +3,6 @@ global stopMinX, stopMaxX, stopMinY, stopMaxY
 from tealight.art import (screen_width, screen_height)
  
 def stopButton():
-    global stopMinX, stopMaxX, stopMinY, stopMaxY  
     color("red")
     rectangle((screen_width - 180), screen_height - 40, 150,30)
     text(screen_width - 85, screen_height - 35, "STOP")
