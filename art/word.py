da034573ed8127c8d3e49b06fbd41b3d81392c6a
amyble <amymylinh.le@hotmail.co.uk>
2015-08-21 09:29:33 +0100

    Closing art/word.py

--- art/word.py
+++ art/word.py
@@ -1,5 +1,5 @@
 import random
-
+from tealight.art import text
 
 def word():
   a = ["Dog", "Cat", "Castle", "Voldemort", "Barack Obama", "Snowman", "Aeroplane", "Apple", "Bible", "Chocolates", "Electricity"]
