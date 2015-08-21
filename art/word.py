7e23a0abbb6ae55883cc5b66f66d3ce1662a0bff
amyble <amymylinh.le@hotmail.co.uk>
2015-08-21 09:42:47 +0100

    Running art/word.py

--- art/word.py
+++ art/word.py
@@ -4,6 +4,6 @@ from tealight.art import screen_width, screen_height
 
 def word():
   a = ["Dog", "Cat", "Castle", "Voldemort", "Barack Obama", "Snowman", "Aeroplane", "Apple", "Bible", "Chocolates", "Electricity"]
-  text(screen_width / 2, screen_height / 2, random.choice(a))
+  text(screen_width /2, screen_height / 2, random.choice(a))
 
 word()
