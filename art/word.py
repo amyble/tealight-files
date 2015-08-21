e8c883646160668c9ec524814e20528981dbf02a
amyble <amymylinh.le@hotmail.co.uk>
2015-08-21 09:28:06 +0100

    Running art/word.py

--- art/word.py
+++ art/word.py
@@ -1,6 +1,6 @@
 import random
 import 
 
-def
-a = ["Dog", "Cat", "Castle", "Voldemort", "Barack Obama", "Snowman", "Aeroplane", "Apple", "Bible", "Chocolates", "Electricity"]
-text(random.choice(a))
+def word():
+  a = ["Dog", "Cat", "Castle", "Voldemort", "Barack Obama", "Snowman", "Aeroplane", "Apple", "Bible", "Chocolates", "Electricity"]
+  text(random.choice(a))
