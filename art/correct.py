773be06739666524bdccd130dd6fd59d1bc6a1f6
amyble <amymylinh.le@hotmail.co.uk>
2015-08-21 09:36:54 +0100

    Running art/correct.py

--- art/correct.py
+++ art/correct.py
@@ -1,13 +1,17 @@
 from tealight.art import (color, line, spot, rectangle, circle, box, image, text, background)
-global stopMaxX, stopMinY, stopMaxY
+global  stopMinY, stopMaxY
 stopMinX = 0
+stopMaxX = 0 
+
 from tealight.art import (screen_width, screen_height)
 
 def stopButton():
-    color("purple")
-    rectangle((screen_width - 180), screen_height - 40, 150,30)
-    text(screen_width - 85, screen_height - 35, "STOP")
-    stopMinX = screen_width - 180
-    stopMaxX = screen_width - 30
-    stopMinY = screen_height - 40
-    stopMaxY = screen_height - 10
+  global  stopMinY, stopMaxY,  stopMinX, stopMaxX
+
+  color("purple")
+  rectangle((screen_width - 180), screen_height - 40, 150,30)
+  text(screen_width - 85, screen_height - 35, "STOP")
+  stopMinX = screen_width - 180
+  stopMaxX = screen_width - 30
+  stopMinY = screen_height - 40
+  stopMaxY = screen_height - 10
