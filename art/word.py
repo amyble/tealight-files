50bca06b235441d62d6a01f9e8f41f2d6d582233
amyble <amymylinh.le@hotmail.co.uk>
2015-08-21 09:41:11 +0100

    Closing art/word.py

--- art/word.py
+++ art/word.py
@@ -1,5 +1,6 @@
 import random
 from tealight.art import text
+from tealight.art import screen_width, screen_height
 
 def word():
   a = ["Dog", "Cat", "Castle", "Voldemort", "Barack Obama", "Snowman", "Aeroplane", "Apple", "Bible", "Chocolates", "Electricity"]
